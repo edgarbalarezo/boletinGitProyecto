95330ef34515741cf76a3152cdd3753cb67af1b4
edgarbalarezo <balarezomar@gmail.com>
2025-11-29 01:09:28 +0100

    Se añadió lectura del primer número

---
+++ src/boletinGit/Calculadora.java
@@ -0,0 +1,11 @@
+package boletinGit;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void ejecutarCalculadora(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer numero:");
+        double numero1= sc.nextDouble();
+    }
+}
