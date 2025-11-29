8646d80996b594175a7e81ba86abbc9a9b1e005f
edgarbalarezo <balarezomar@gmail.com>
2025-11-29 01:29:48 +0100

    Mostrar seleción de operación

--- src/boletinGit/Calculadora.java
+++ src/boletinGit/Calculadora.java
@@ -7,5 +7,11 @@ public class Calculadora {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese el primer numero:");
         double numero1= sc.nextDouble();
+        System.out.println("Ingrese la operación:");
+        System.out.println("\t1 - Suma" +
+                "\n\t2 - Resta" +
+                "\n\t3 - Multiplicación" +
+                "\n\t4 - División" +
+                "\n\t0 - Volver al menú\n");
     }
 }
