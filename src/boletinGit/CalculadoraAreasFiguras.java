fead70be0c6ed3cf8f93f126047c224eff8df17c
edgarbalarezo <balarezomar@gmail.com>
2025-11-30 02:01:54 +0100

    Crear menú de selección de figura en la función ejecutarCalculadoraFiguras

---
+++ src/boletinGit/CalculadoraAreasFiguras.java
@@ -0,0 +1,13 @@
+package boletinGit;
+
+import java.util.Scanner;
+
+public class CalculadoraAreasFiguras {
+    public static void ejecutarCalculadoraFiguras() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Seleccione la figura:");
+        System.out.println("1 - Círculo");
+        System.out.println("2 - Cuadrado");
+        System.out.println("3 - Triángulo");
+    }
+}
